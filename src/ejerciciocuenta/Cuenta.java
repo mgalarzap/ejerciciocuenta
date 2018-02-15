23f4eed7bf1351a675f5cf0efa5ee988af5e542e
Estudiante <Estudiante@10.203.1.226>
2018-02-15 09:18:03 -0500

    ejercicio 1 de clases y objetos

---
+++ src/ejerciciocuenta/Cuenta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocuenta;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Cuenta {
+    public int numero;
+    
+    public int clave;
+    public String propietario;
+    public double saldo;
+    
+
+}
